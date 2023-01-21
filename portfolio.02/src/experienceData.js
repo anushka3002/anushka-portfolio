29206000a9762573fb55b518331c2b4d3cfe7c93
anushka-chace <anushka.priya@lockrfinance.com>
2023-01-21 10:04:30 +0530

    added experience description

--- portfolio.02/src/experienceData.js
+++ portfolio.02/src/experienceData.js
@@ -12,7 +12,7 @@ export const experience_projects = [
      title: "Letslockr",
      subtitle: "TECH STACK: HTML | CSS | Tailwind CSS | JavaScript | NextJs | ReactJs",
      description:
-       "Lockr is building banking website for eCommerce Merchants and Startups.",
+       "Lockr is building banking website for eCommerce Merchants and Startups. User can use cash or card to deposit or withdraw amount.",
      image: "./Letslockr.png",
      link: "https://www.letslockr.com/",
      blog:""
@@ -21,7 +21,7 @@ export const experience_projects = [
      title: "Lockr pay app",
      subtitle: "TECH STACK: Flutter | Dart",
      description:
-       "Lockr pay app is building banking app for eCommerce Merchants and Startups.",
+       "Lockr pay app is building banking app for eCommerce Merchants and Startups. User can use cash or card to deposit or withdraw amount. It has a feature to freeze or unfreeze card.",
      image: "./lockrpay.png",
      link:"",
      blog:""
@@ -30,7 +30,7 @@ export const experience_projects = [
      title: "Lockr Finance",
      subtitle: "TECH STACK: HTML | CSS | Tailwind CSS | JavaScript | NextJs | ReactJs",
      description:
-       "A crypto lending project where in a user can borrow USDC against their BTC / ETH",
+       "Lockr Finance is a crypto lending project that allows users to borrow USDC in exchange for their BTC / ETH. Lockr is the best place to borrow against Bitcoin and Ether without spending it.",
      image: "./LockrFinance.png",
      link: "https://lockrfinance.com/",
    },
